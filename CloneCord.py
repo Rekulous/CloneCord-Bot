90e848131a4ac3ce172a99bfe30f36c34bc28e55
Rekulous <79665689+Rekulous@users.noreply.github.com>
2021-09-17 11:21:42 -0400

    Update CloneCord.py

--- CloneCord.py
+++ CloneCord.py
@@ -40,7 +40,7 @@ else:
 # Sweet ass bot logging
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename="clonecord.log", encoding="utf-8", mode="w")
+handler = logging.FileHandler(filename="bot.log", encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 
